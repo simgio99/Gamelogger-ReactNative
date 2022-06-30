0a475980d30545c620c19ebecd8646ce031aac01
Breiart <aversanamichele@gmail.com>
2022-06-30 12:28:25 +0200

    porcorio

--- App.tsx
+++ App.tsx
@@ -33,7 +33,7 @@ function HomeScreen() {
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    
+      <Text>Podcorio</Text>
     </View>
   );
 }
